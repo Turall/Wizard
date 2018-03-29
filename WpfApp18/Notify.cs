00824a0fd69cecbbab98801117e515abb6908fa4
Tural Muradov <Tural_M@hotmail.com>
2018-03-29 21:40:26 +0400

    Add project files.

---
+++ WpfApp18/Notify.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp18
+{
+    class Notify : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName]string notify = "")
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(notify));
+            }
+        }
+    }
+}
---
+++ WpfApp18/Students.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp18
+{
+    class Students : Notify
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Age { get; set; }
+
+        private bool _checked;
+        public bool Checked
+        {
+            get { return _checked; }
+            set { _checked = value;
+                if(StudentChackedChange != null)
+                {
+                    StudentChackedChange(this, new EventArgs());
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public event EventHandler StudentChackedChange;
+    }
+}
